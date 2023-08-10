6c3bf8e9f335983af771657e385210ec903d5d19
Jorge-DevOps <jorgedevops20@gmail.com>
2023-08-10 09:01:08 -0500

    fix:(Ajust from client Dogtor kat)

--- Programing/BackEnd/DevContainer-python/gcloud.sh
+++ Programing/BackEnd/DevContainer-python/gcloud.sh
@@ -1,1 +1,3 @@
 gcloud builds submit --tag [IMAGE] /home/houndlybot/chatbot && gcloud run deploy chatbot --image [IMAGE]
+
+git pull && gcloud run deploy chatbot  --platform managed --region northamerica-northeast1
